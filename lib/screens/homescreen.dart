c434c18218e003a3b2aa2d83f52596815bcc34f5
Isma Meinawati <Ismameinawati@gmail.com>
2024-11-29 00:03:11 -0800

    pertemuan 8

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_mobile_5/models/destination_model.dart';
+import 'package:wisata_mobile_5/screens/detaildestinasiscreen.dart';
 import 'package:wisata_mobile_5/utils/const.dart';
 import 'package:wisata_mobile_5/widgets/popular_destination.dart';
 import 'package:wisata_mobile_5/widgets/rekomendasi_destination.dart';
@@ -13,102 +14,155 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   int selectedPage = 0;
-  List<IconData>icon=[
+  List<IconData> icon = [
     Icons.home_filled,
     Icons.bookmark_border_rounded,
     Icons.shopping_cart_outlined,
     Icons.person_outline_outlined
   ];
 
-  List<TravelDestination>popular =listDestination.where((Element)=> Element.category == 'popular').toList();
+  List<TravelDestination> popular = listDestination
+      .where((Element) => Element.category == 'popular')
+      .toList();
 
-   List<TravelDestination>rekomendasi =listDestination.where((Element)=> Element.category == 'rekomendasi').toList();
-
+  List<TravelDestination> rekomendasi = listDestination
+      .where((Element) => Element.category == 'rekomendasi')
+      .toList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Column(
-        children: [buildAppBar(),
-        SizedBox(height: 20),
-        Padding(
-          padding:EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Tempat Populer',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: blueTextColor,
-                ),
-              )
-            ],
-          ),
+        children: [
+          buildAppBar(),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Tempat Populer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: blueTextColor,
+                  ),
+                )
+              ],
+            ),
           ),
           SizedBox(height: 20),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             padding: EdgeInsets.only(bottom: 20, left: 20),
             child: Row(
-              children:List.generate(popular.length,
-              (index)=> Padding(
-                padding: EdgeInsets.only(right: 15),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: PopularDestination(
-                    destination: popular[index]),
-                ),)
-              ),
+              children: List.generate(
+                  popular.length,
+                  (index) => Padding(
+                        padding: EdgeInsets.only(right: 15),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => Detaildestinasiscreen(
+                                  destination: popular[index])
+                              )
+                              );
+                          },
+                          child:
+                              PopularDestination(destination: popular[index]),
+                        ),
+                      )),
             ),
           ),
           SizedBox(height: 20),
-           Padding(
-          padding:EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Rekomendasi Untuk Kamu',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: blueTextColor,
-                ),
-              )
-            ],
-          ),
-          ),
-            SizedBox(height: 20),
-            Expanded(child:
-              SingleChildScrollView(
-            scrollDirection: Axis.vertical,
+          Padding(
             padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              children:List.generate(rekomendasi.length,
-              (index)=> Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: RekomendasiDestination(
-                    destination: rekomendasi[index]),
-                ),)
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Rekomendasi Untuk Kamu',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: blueTextColor,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: List.generate(
+                    rekomendasi.length,
+                    (index) => Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => Detaildestinasiscreen(
+                                  destination: rekomendasi[index])
+                              )
+                              );
+                            },
+                            child: RekomendasiDestination(
+                                destination: rekomendasi[index]),
+                          ),
+                        )),
               ),
             ),
           ),
-             ),
-
-
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 18),
+                  decoration: BoxDecoration(
+                    color: kButtonColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: List.generate(
+                          icon.length,
+                          (index) => GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    selectedPage = index;
+                                  }); 
+                                },
+                                child:Icon(
+                                  icon[index],
+                                  size: 32,
+                                  color: selectedPage == index 
+                                        ?Colors.white
+                                        : Colors.white.withOpacity(0.4),
+                                ),
+                              ))),
+                ),
+                SizedBox(height: 25)
+              ],
+            ),
+          ),
         ],
-
       ),
     );
   }
 }
 
-Widget buildAppBar(){
+Widget buildAppBar() {
   return Container(
     height: 120,
     decoration: BoxDecoration(
@@ -117,18 +171,17 @@ Widget buildAppBar(){
         bottomLeft: Radius.circular(25),
         bottomRight: Radius.circular(25),
       ),
-      boxShadow:[
+      boxShadow: [
         BoxShadow(
           color: Colors.grey.withOpacity(0.7),
           spreadRadius: 5,
           blurRadius: 10,
-          offset: Offset(0,5),
-
+          offset: Offset(0, 5),
         ),
-        ],
-
+      ],
     ),
-    child: SafeArea(child: Center(
+    child: SafeArea(
+        child: Center(
       child: Padding(
         padding: EdgeInsets.only(top: 20, left: 15, right: 15),
         child: buildSearchButton(),
@@ -137,19 +190,19 @@ Widget buildAppBar(){
   );
 }
 
-Widget buildSearchButton(){
+Widget buildSearchButton() {
   return Container(
     padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
     decoration: BoxDecoration(
-      color: kButtonColor,
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: [BoxShadow(
-        color: Colors.grey.withOpacity(0.8),
-        spreadRadius: 2,
-        blurRadius: 10,
-        offset: Offset(0,5)
-      ),]
-    ),
+        color: kButtonColor,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey.withOpacity(0.8),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: Offset(0, 5)),
+        ]),
     child: TextField(
       decoration: InputDecoration(
         hintText: 'Cari Destinasi...',
@@ -160,15 +213,11 @@ Widget buildSearchButton(){
         prefixIcon: Icon(Icons.search, color: Colors.white54, size: 28),
         border: InputBorder.none,
         contentPadding: EdgeInsets.symmetric(vertical: 12),
-
       ),
-
       style: TextStyle(
         color: Colors.white,
         fontSize: 18,
       ),
     ),
-
-
   );
 }
